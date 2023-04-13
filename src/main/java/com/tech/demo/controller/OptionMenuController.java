932680c16e9cf9cb40a2b342c06fa76b1888d405
gabusy07 <jin_thecraziest@live.com>
2023-04-12 21:04:38 -0300

    fix cross origin in sidebar

--- src/main/java/com/tech/demo/controller/OptionMenuController.java
+++ src/main/java/com/tech/demo/controller/OptionMenuController.java
@@ -6,6 +6,7 @@ import com.tech.demo.service.IOptionMenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,12 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/menu")
+@CrossOrigin(origins="${client_url}", maxAge = 3600)
 public class OptionMenuController {
     private final IOptionMenuService optionMenuService;
 
     @GetMapping
     public ResponseEntity<Flux<OptionsMenu>> getOptions(){
-        return new ResponseEntity<>(optionMenuService.getAllOptions(), HttpStatus.OK);
+        return new ResponseEntity(optionMenuService.getAllOptions(), HttpStatus.OK);
     }
 }
