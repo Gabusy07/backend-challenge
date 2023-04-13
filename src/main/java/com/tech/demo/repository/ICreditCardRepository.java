7f2b12482ae7622dfe68fd772cc0833d52e3c4a0
gabusy07 <jin_thecraziest@live.com>
2023-04-13 10:13:41 -0300

    a

--- src/main/java/com/tech/demo/repository/ICreditCardRepository.java
+++ src/main/java/com/tech/demo/repository/ICreditCardRepository.java
@@ -4,6 +4,9 @@ import com.tech.demo.document.CreditCard;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface ICreditCardRepository extends ReactiveMongoRepository<CreditCard, String> {
+
 }
